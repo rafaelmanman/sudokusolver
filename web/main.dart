e6671aed7e2b2030866739526ea12f4af482ac5b
Alexander Chang <rafalemanman@gmail.com>
2021-01-16 10:53:44 -0800

    Started website

---
+++ web/main.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+TableElement sudoku_grid;
+
+void main() {
+  var header = querySelector("#header");
+  var sudoku_grid = querySelector("#sudoku-grid");
+  print(sudoku_grid);
+  print(header);
+}
